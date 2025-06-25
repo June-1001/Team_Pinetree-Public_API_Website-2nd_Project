cc453a109ae61a074a1ab3d94dc4eef04ed4c0c0
고건우 <junepark.dev@gmail.com>
2025-06-25 16:11:35 +0900

    deleted test

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import GetData from "./API/getData";
-
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.StrictMode>
-    <GetData item="cultural" />
-    <GetData item="health" />
-    <GetData item="weather" />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
 
 reportWebVitals();
