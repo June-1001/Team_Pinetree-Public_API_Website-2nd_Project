ebfd9ef824408e1f9ee151cb46f98e46b5244e8a
고건우 <junepark.dev@gmail.com>
2025-06-25 14:12:34 +0900

    tested getData

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { GetRequestUrl } from "./key/urlKey";
+import GetData from "./API/getData";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<React.StrictMode></React.StrictMode>);
-
-console.log(GetRequestUrl("cultural"));
-console.log(GetRequestUrl("health"));
-console.log(GetRequestUrl("weather"));
+root.render(
+  <React.StrictMode>
+    <GetData item="cultural" />
+    <GetData item="health" />
+    <GetData item="weather" />
+  </React.StrictMode>
+);
 
 reportWebVitals();
