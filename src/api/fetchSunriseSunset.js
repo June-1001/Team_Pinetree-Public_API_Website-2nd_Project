07e8fa5f918aeceabb5bb3b64e7525e2f532fce9
고건우 <junepark.dev@gmail.com>
2025-07-18 14:15:13 +0900

    Update fetchSunriseSunset.js

--- src/api/fetchSunriseSunset.js
+++ src/api/fetchSunriseSunset.js
@@ -5,17 +5,17 @@ const SERVICE_KEY = `Fn8nBDNNZn0hIRp85JkxCQhQUBSuaxYLC11J5xocF5WqxiLpogKvhdaKPkf
 export async function fetchSunriseSunset({ lat, lon, date }) {
   if (!lat || !lon || !date) return null;
 
-  const locdate = date.replace(/-/g, ""); 
-  const latitude = parseFloat(lat).toFixed(7);    // 실수형반환
+  const locdate = date.replace(/-/g, "");
+  const latitude = parseFloat(lat).toFixed(7); // 실수형반환
 
-  
-  const longitude = parseFloat(lon).toFixed(7);  
-  const url = `https://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getLCRiseSetInfo?` +
-              `serviceKey=${encodeURIComponent(SERVICE_KEY)}` +
-              `&locdate=${locdate}` +
-              `&longitude=${longitude}` +
-              `&latitude=${latitude}` +
-              `&dnYn=Y&_type=json`;
+  const longitude = parseFloat(lon).toFixed(7);
+  const url =
+    `https://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getLCRiseSetInfo?` +
+    `serviceKey=${encodeURIComponent(SERVICE_KEY)}` +
+    `&locdate=${locdate}` +
+    `&longitude=${longitude}` +
+    `&latitude=${latitude}` +
+    `&dnYn=Y&_type=json`;
 
   try {
     console.log("[일출/일몰 API 요청 URL]", url);
